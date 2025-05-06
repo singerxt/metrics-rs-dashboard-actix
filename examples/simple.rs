f09f418b33b2a0567c707a019b0ac3d794b3f4d0
Mateusz Śpiewak <mat.spiewak@gmail.com>
2025-05-07 00:17:02 +0200

    chore: Add metrics recorder and Prometheus integration.

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,4 +1,8 @@
+use std::sync::Arc;
+
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use log::info;
+use metrics_actix_dashboard::create_metrics_actx_scope;
 
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello from Actix-Web!")
@@ -6,11 +10,13 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting server at http://127.0.0.1:8080");
-
+    env_logger::init();
+    info!("Starting Actix-Web server with metrics at /metrics");
     HttpServer::new(|| {
+        let metrics_actix_dashboard = create_metrics_actx_scope().unwrap();
         App::new()
             .route("/", web::get().to(hello))
+            .service(metrics_actix_dashboard)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,47 @@
+use std::sync::{Mutex, OnceLock};
 
-pub async fn hello_world() {
-    println!("Hello, world! from lib");
+use actix_web::{web, HttpResponse, Responder, Scope};
+use anyhow::{Context, Result};
+use log_once::debug_once;
+use metrics_prometheus::failure::strategy;
+use metrics_util::layers::FanoutBuilder;
+
+static IS_CONFIGURED: OnceLock<Mutex<bool>> = OnceLock::new();
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello from Actix-Web!")
+}
+
+fn configure_metrics_recorders_once() -> Result<()> {
+    let mutex = IS_CONFIGURED.get_or_init(|| Mutex::new(false));
+    let mut is_ok = mutex
+        .lock()
+        .map_err(|e| anyhow::anyhow!("Mutex poisoned: {}", e))
+        .with_context(|| "Unable to lock IS_CONFIGURED")?;
+
+    if *is_ok {
+        debug_once!("You have already configured the metrics recorder. This is a no-op. Multiple configuration attempts are safe because only the first one takes effect, preventing duplicate registrations.");
+        return Ok(());
+    }
+
+    *is_ok = true;
+
+    let prometeus_recorder = metrics_prometheus::Recorder::builder()
+        .with_failure_strategy(strategy::NoOp)
+        .build();
+
+    let fanout = FanoutBuilder::default()
+        .add_recorder(prometeus_recorder)
+        .build();
+
+    metrics::set_global_recorder(fanout).expect("Unable to register a recorder.Did you call this function multiple times?");
+
+    Ok(())
+}
+
+pub fn create_metrics_actx_scope() -> Result<Scope> {
+    configure_metrics_recorders_once()?;
+    let scope = web::scope("/metrics")
+        .service(web::resource("/").route(web::get().to(hello)));
+    Ok(scope)
 }
