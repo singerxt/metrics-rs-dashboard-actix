bc67aa7d16bb45b43742a6c06340f9499914ee5e
Mateusz Śpiewak <mat.spiewak@gmail.com>
2025-05-08 20:55:11 +0200

    chore: added MetricBuffer

--- public/app/common/MetricBuffer.js
+++ public/app/common/MetricBuffer.js
@@ -1,31 +1,31 @@
 class MetricBuffer {
-	#buffer = new Map();
-	// How many entries per metric
-	#bufferSize = 10;
+  #buffer = new Map();
+  // Maximum number of entries per metric
+  #bufferSize = 10;
 
-	constructor(bufferSize) {
-		if (bufferSize) {
-			this.#bufferSize = bufferSize;
-		}
-	}
+  constructor(bufferSize) {
+    if (bufferSize) {
+      this.#bufferSize = bufferSize;
+    }
+  }
 
-	addMetrics(metrics) {
-		for (const sample of metrics) {
-			const currentSample = this.#buffer.get(sample.name);
-			if (!currentSample) {
-				this.#buffer.set(sample.name, sample);
+  addMetrics(metrics) {
+    for (const sample of metrics) {
+      const currentSample = this.#buffer.get(sample.name);
+      if (!currentSample) {
+        this.#buffer.set(sample.name, sample);
         return;
-			}
+      }
 
-			if (currentSample.metrics.length >= this.#bufferSize) {
+      if (currentSample.metrics.length >= this.#bufferSize) {
         currentSample.metrics.shift();
       }
 
-			const newMetrics = currentSample.metrics.concat(sample.metrics);
+      const newMetrics = currentSample.metrics.concat(sample.metrics);
       currentSample.metrics = newMetrics;
-			this.#buffer.set(sample.name, currentSample);
-		}
-	}
+      this.#buffer.set(sample.name, currentSample);
+    }
+  }
 
   logMetrics() {
     for (const [name, sample] of this.#buffer.entries()) {
